5d1310d8d1dfa72f968db77fbb3921eaa956cf0b
bertonfederico <federicoberton.fb@gmail.com>
2024-07-24 22:35:32 +0200

    Refactored initial code

---
+++ new_eeg_data_endpoint/docker/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request
+import requests
+
+app = Flask(__name__)
+
+prediction_url = 'https://prediction_service/predict'
+saving_url = 'https://db-queries-app-service/eeg_chunks/add' 
+
+@app.route('/new_eeg_data_endpoint', methods=['POST'])
+def new_eeg_data_endpoint():
+    fk_user = request.get_json()['fk_user']
+    eeg_data = request.get_json()['eeg_data']
+    register_timestamp = request.get_json()['register_timestamp']
+
+    prediction_payload = {'eeg_data': eeg_data}
+    prediction_response = requests.post(prediction_url, json=prediction_payload)
+    if prediction_response.status_code != 201:
+        return jsonify({'status': 'error', 'message': 'Failed to predict EEG status'}), prediction_response.status_code
+
+    saving_payload = {
+        'fk_user': fk_user,
+        'eeg_data': eeg_data,
+        'epilepsy_prediction_probability': prediction_response.json(),
+        'register_timestamp': register_timestamp
+
+    }
+    saving_response = requests.post(saving_url, json=saving_payload)
+    if saving_response.status_code != 201:
+        return jsonify({'status': 'error', 'message': 'Failed to save EEG status'}), prediction_response.status_code
+    
+    return jsonify({'status': 'success', "message": "Completed", "response": prediction_response.json()}), 201
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
--- test/test.sh
+++
@@ -1,2 +0,0 @@
-EXTERNAL_IP=$(kubectl get svc flask-app-service -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-curl -X POST $EXTERNAL_IP/message -H "Content-Type: application/json" -d '{"message": "Hello, Kubernetes!"}'
