0db3207dc6025c2e34ec223ea8ebb5f5ab332bb6
bertonfederico <federicoberton.fb@gmail.com>
2024-07-26 17:06:17 +0200

    Fixed methods name in queries service

--- db_queries/docker/app.py
+++ db_queries/docker/app.py
@@ -49,7 +49,7 @@ def add_egg_data_prediction_chunk():
     return jsonify({'status': 'success'}), 201
 
 @app.route('/eeg_chunks/get_user_list', methods=['GET'])
-def get_egg_data_prediction_chunks():
+def get_user_egg_data_prediction_chunks():
     fk_user = request.args.get('fk_user')
     connection = mysql.connector.connect(**db_config)
     cursor = connection.cursor(dictionary=True)
@@ -70,7 +70,7 @@ def get_egg_data_prediction_chunks():
     return jsonify(results)
 
 @app.route('/eeg_chunks/get_last_chunks_list', methods=['GET'])
-def get_egg_data_prediction_chunks():
+def get_last_egg_data_prediction_chunks():
     connection = mysql.connector.connect(**db_config)
     cursor = connection.cursor(dictionary=True)
     cursor.execute("SELECT ed.fk_user, ed.eeg_data, ed.epilepsy_prediction_probability, ed.register_timestamp FROM eeg_data_table ed INNER JOIN (SELECT fk_user, MAX(register_timestamp) AS max_timestamp FROM eeg_data_table GROUP BY fk_user) sub ON ed.fk_user = sub.fk_user AND ed.register_timestamp = sub.max_timestamp;")
