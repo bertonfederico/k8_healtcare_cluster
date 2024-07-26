7af46ee6468639d93b84d3906d796fdd71d6c447
bertonfederico <federicoberton.fb@gmail.com>
2024-07-26 13:28:09 +0200

    Sending data and prediction to web page service

--- new_eeg_data_endpoint/docker/app.py
+++ new_eeg_data_endpoint/docker/app.py
@@ -4,7 +4,8 @@ import requests
 app = Flask(__name__)
 
 prediction_url = 'https://epilepsy-prediction-service/predict'
-saving_url = 'https://db-queries-service/eeg_chunks/add' 
+saving_url = 'https://db-queries-service/eeg_chunks/add'
+healt_care_center_url = 'https://healt-center-web-page-service/update' 
 
 @app.route('/', methods=['POST'])
 def new_eeg_data_endpoint():
@@ -12,11 +13,13 @@ def new_eeg_data_endpoint():
     eeg_data = request.get_json()['eeg_data']
     register_timestamp = request.get_json()['register_timestamp']
 
+    # POST request for prediction
     prediction_payload = {'eeg_data': eeg_data}
     prediction_response = requests.post(prediction_url, json=prediction_payload)
     if prediction_response.status_code != 201:
         return jsonify({'status': 'error', 'message': 'Failed to predict EEG status'}), prediction_response.status_code
 
+    # POST request for saving data in database
     saving_payload = {
         'fk_user': fk_user,
         'eeg_data': eeg_data,
@@ -27,6 +30,11 @@ def new_eeg_data_endpoint():
     saving_response = requests.post(saving_url, json=saving_payload)
     if saving_response.status_code != 201:
         return jsonify({'status': 'error', 'message': 'Failed to save EEG status'}), prediction_response.status_code
+    
+    # POST request for updating healt center web page
+    web_page_monitoring_response = requests.post(healt_care_center_url, json=saving_payload)
+    if web_page_monitoring_response.status_code != 201:
+        return jsonify({'status': 'error', 'message': 'Failed to show EEG chunk in web page'}), web_page_monitoring_response.status_code
     
     return jsonify({'status': 'success', "message": "Completed", "response": prediction_response.json()}), 201
 
