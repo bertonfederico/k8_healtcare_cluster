de16a0cce3cba59e16fc40a2ac0cb7d5260a725d
bertonfederico <federicoberton.fb@gmail.com>
2024-08-02 00:55:26 +0200

    Deleted old directory

--- db_queries/docker/docker_builder.sh
+++
@@ -1,4 +0,0 @@
-docker build -t fberton98/db_queries:latest .
-docker push fberton98/db_queries:latest
-#docker run -p 5000:5000 fberton98/db_queries:latest
-#curl http://127.0.0.1:5000/app/eeg_chunks/get_list
