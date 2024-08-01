be3f94136d72021cb7a823ca374b947af718ad1c
bertonfederico <federicoberton.fb@gmail.com>
2024-08-02 01:01:06 +0200

    Created new endpoint for heart beat

---
+++ new_heartbeat_data_endpoint/docker/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+import requests
+
+app = Flask(__name__)
+
+saving_url = 'http://db-connection-service/heartbeat_chunks/add'
+
+@app.route('/', methods=['POST'])
+def new_heartbeat_endpoint():
+    json_data = request.get_json()['data']
+    heartbeat_data = json_data['metrics']['data']
+
+    # POST request for saving data in database
+    saving_payload = {
+        'heartbeat_data': heartbeat_data
+    }
+    saving_response = requests.post(saving_url, json=saving_payload)
+    if saving_response.status_code != 201:
+        return jsonify({'status': 'error', 'message': 'Failed to save heartbeat status'}), saving_response.status_code
+    
+    return jsonify({'status': 'success', "message": "Completed"}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ new_heartbeat_data_endpoint/docker/docker_builder.sh
@@ -0,0 +1,3 @@
+docker build -t fberton98/new_heartbeata_endpoint:latest .
+docker push fberton98/new_heartbeata_endpoint:latest
+#docker run -p 5000:5000 fberton98/new_heartbeata_endpoint:latest
