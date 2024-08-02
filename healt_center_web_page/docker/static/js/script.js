7aa2d4d8918fb4ebe693656991cd2ca392f7ecce
bertonfederico <federicoberton.fb@gmail.com>
2024-08-02 18:22:26 +0200

    Completed web page

--- healt_center_web_page/docker/static/js/script.js
+++ healt_center_web_page/docker/static/js/script.js
@@ -74,7 +74,16 @@ function addOrUpdateEegRow(fk_id, eegData, probability, datetime) {
     }
 }
 
-function addOrUpdateHeartRow(fk_id, heartbeat_values, register_timestamps) {
+function getColorForHeartbeatValue(value) {
+    min = 20;
+    max = 150;
+    const normalized = (value - min) / (max - min);
+    const green = Math.floor(255 * (1 - normalized));
+    const red = Math.floor(255 * normalized);
+    return `rgb(${red}, ${green}, 0)`;
+}
+
+function addOrUpdateHeartRow(fk_id, heartbeat_values, register_timestamps, mean, higher, lower) {
     const table = document.getElementById('heartTable').getElementsByTagName('tbody')[0];
     let row = Array.from(table.rows).find(row => row.cells[0].innerText === fk_id.toString());
 
@@ -86,6 +95,26 @@ function addOrUpdateHeartRow(fk_id, heartbeat_values, register_timestamps) {
         first.style.setProperty('height', '200px');
         const second = row.insertCell(1);
         second.innerHTML = '<canvas></canvas>';
+        const third = row.insertCell(2);
+        const container = document.createElement('div');
+        container.className = 'cell-content';
+        const line1 = document.createElement('div');
+        line1.className = 'line';
+        line1.innerText = "Higher: " + higher;
+        line1.style.backgroundColor = getColorForHeartbeatValue(higher);
+        container.appendChild(line1);
+        const line2 = document.createElement('div');
+        line2.className = 'line';
+        line2.innerText = "Mean: " + mean;
+        line2.style.backgroundColor = getColorForHeartbeatValue(mean);
+        container.appendChild(line2);
+        const line3 = document.createElement('div');
+        line3.className = 'line';
+        line3.innerText = "Lower: " + lower;
+        line3.style.backgroundColor = getColorForHeartbeatValue(lower);
+        container.appendChild(line3);
+        third.appendChild(container);
+        third.style.setProperty('width', '13%');
         const ctx = row.cells[1].getElementsByTagName('canvas')[0].getContext('2d');
         hearbeat_charts[fk_id] = new Chart(ctx, {
             type: 'line',
@@ -145,7 +174,11 @@ async function fetchHeartBeatData() {
     for (let fk_user in data) {
         const heartbeat_values = data[fk_user].heartbeat_values;
         const register_timestamps = data[fk_user].register_timestamps;
-        addOrUpdateHeartRow(fk_user, heartbeat_values, register_timestamps);
+        const sum = heartbeat_values.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+        const average = Math.floor(sum / heartbeat_values.length);
+        const higher = Math.max(...heartbeat_values);
+        const lower = Math.min(...heartbeat_values);
+        addOrUpdateHeartRow(fk_user, heartbeat_values, register_timestamps, average, higher, lower);
     }
 }
 
