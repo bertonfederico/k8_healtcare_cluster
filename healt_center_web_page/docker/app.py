21c4fe79df947b0a6b380605fe2ab3c2912dfa30
bertonfederico <federicoberton.fb@gmail.com>
2024-08-02 14:03:30 +0200

    Added hear beats in web page

--- healt_center_web_page/docker/app.py
+++ healt_center_web_page/docker/app.py
@@ -6,18 +6,20 @@ import time
 app = Flask(__name__)
 
 eeg_data_saving = {}
+heartbeat_data_saving = {}
 
-db_url = 'http://db-connection-service/eeg_chunks/get_last_chunks_list'
+db_eeg_url = 'http://db-connection-service/eeg_chunks/get_last_chunks_list'
+db_heartbeat_url = 'http://db-connection-service/heartbeat_chunks/get_last_chunks_list'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-def send_periodic_requests():
+def send_periodic_eeg_requests():
     global eeg_data_saving
     while True:
         try:
-            response = requests.get(db_url)
+            response = requests.get(db_eeg_url)
             if response.status_code == 200:
                 eeg_data_saving = response.json()
             else:
@@ -26,10 +28,29 @@ def send_periodic_requests():
             raise SystemError("Error in periodic request")
         time.sleep(5)
 
-@app.route('/data', methods=['GET'])
+
+def send_periodic_heartbeat_requests():
+    global heartbeat_data_saving
+    while True:
+        try:
+            response = requests.get(db_heartbeat_url)
+            if response.status_code == 200:
+                heartbeat_data_saving = response.json()
+            else:
+                raise SystemError("Error in periodic request")
+        except requests.RequestException as e:
+            raise SystemError("Error in periodic request")
+        time.sleep(5)
+
+@app.route('/eeg_data', methods=['GET'])
 def get_data():
     return eeg_data_saving
 
+@app.route('/heartbeat_data', methods=['GET'])
+def get_data():
+    return heartbeat_data_saving
+
 if __name__ == '__main__':
-    threading.Thread(target=send_periodic_requests, daemon=True).start()
+    threading.Thread(target=send_periodic_eeg_requests, daemon=True).start()
+    threading.Thread(target=send_periodic_heartbeat_requests, daemon=True).start()
     app.run(host='0.0.0.0', port=5000)
--- healt_center_web_page/docker/static/js/script.js
+++ healt_center_web_page/docker/static/js/script.js
@@ -1,6 +1,6 @@
 const charts = {}
 
-function addOrUpdateRow(fk_id, eegData, probability, datetime) {
+function addOrUpdateEegRow(fk_id, eegData, probability, datetime) {
     const table = document.getElementById('eegTable').getElementsByTagName('tbody')[0];
     let row = Array.from(table.rows).find(row => row.cells[0].innerText === fk_id.toString());
 
@@ -73,12 +73,79 @@ function addOrUpdateRow(fk_id, eegData, probability, datetime) {
     }
 }
 
-async function fetchData() {
-    const response = await fetch('/data');
-    const data = await response.json();
-    data.forEach(item => addOrUpdateRow(item.fk_user, JSON.parse(item.eeg_data), item.epilepsy_prediction_probability, item.register_timestamp));
+function addOrUpdateHeartRow(fk_id, heartbeat_values, register_timestamps) {
+    const table = document.getElementById('heartTable').getElementsByTagName('tbody')[0];
+    let row = Array.from(table.rows).find(row => row.cells[0].innerText === fk_id.toString());
+
+    if (!row) {
+        row = table.insertRow();
+        const first = row.insertCell(0);
+        first.innerText = fk_id;
+        first.style.setProperty('width', '13%');
+        first.style.setProperty('height', '200px');
+        const second = row.insertCell(1);
+        second.innerHTML = '<canvas></canvas>';
+        const ctx = row.cells[1].getElementsByTagName('canvas')[0].getContext('2d');
+        charts[fk_id] = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: register_timestamps,
+                datasets: [{
+                    label: 'Heartbeat data',
+                    data: heartbeat_values,
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1,
+                    fill: false,
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    x: {
+                        type: 'time',
+                        title: {
+                            display: true,
+                            text: 'Timestamp'
+                        }
+                    },
+                    y: {
+                        min: 0,
+                        max: 150,
+                        title: {
+                            display: true,
+                            text: 'Heart beat'
+                        }
+                    }
+                }
+            }
+        });
+    } else {
+        const chart = charts[fk_id]
+        chart.data.labels = register_timestamps;
+        chart.data.datasets[0].data = eegData;
+        chart.update();
+    }
 }
 
-setInterval(fetchData, 5000);
+async function fetchEegData() {
+    const response = await fetch('/eeg_data');
+    const data = await response.json();
+    data.forEach(item => addOrUpdateEegRow(item.fk_user, JSON.parse(item.eeg_data), item.epilepsy_prediction_probability, item.register_timestamp));
+}
 
-fetchData();
+async function fetchHeartBeatData() {
+    const response = await fetch('/heartbeat_data');
+    const data = await response.json();
+    for (let fk_user in data) {
+        const heartbeat_values = data[fk_user].heartbeat_values;
+        const register_timestamps = data[fk_user].register_timestamps;
+        addOrUpdateHeartRow(fk_user, JSON.parse(heartbeat_values), JSON.parse(register_timestamps));
+    }
+}
+
+setInterval(fetchEegData, 5000);
+setInterval(fetchHeartBeatData, 5000);
+
+fetchEegData();
+fetchHeartBeatData();
