ddcffa31fad7f8f2688265b077b31ea7c382545d
bertonfederico <federicoberton.fb@gmail.com>
2024-08-30 00:19:14 +0200

    Adding test phase

--- _2_services/epilepsy_prediction/docker/app.py
+++ _2_services/epilepsy_prediction/docker/app.py
@@ -7,6 +7,7 @@ model = Model.load("NeuralNetwork.pmml")
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    #com
     eeg_data = request.get_json()['eeg_data']
     post_data = {}
     i = 0
