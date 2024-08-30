4ae750530385580e7e9fcdf5610a97049d75cdff
bertonfederico <federicoberton.fb@gmail.com>
2024-08-30 18:18:16 +0200

    Testing unit-smoke tests for DB connection

--- _2_services/db_connection/docker/app.py
+++ _2_services/db_connection/docker/app.py
@@ -16,6 +16,7 @@ db_config = {
 
 @app.route('/eeg_chunks/add', methods=['POST'])
 def add_egg_data_prediction_chunk():
+    #cmd1
     data = request.json
     connection = mysql.connector.connect(**db_config)
     cursor = connection.cursor()
