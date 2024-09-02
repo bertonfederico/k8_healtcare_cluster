56df37f17f514522ad198ef1a560abeb45c9d0da
bertonfederico <federicoberton.fb@gmail.com>
2024-09-02 15:17:31 +0200

    Refactored initial commands for cluster creation

--- _0_run_cluster/_0_run.sh
+++ _0_run_cluster/_0_run.sh
@@ -1,3 +1,15 @@
+###########################################
+########## Initializing cluster ###########
+###########################################
+
+
+# Creating MetalLB
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.3/config/manifests/metallb-native.yaml
+kubectl apply -f ipaddresspool.yaml
+kubectl apply -f l2advertisement.yaml 
+
+
+
 # Updating docker images
 cd epilepsy_prediction/docker/
 sh docker_builder.sh
@@ -21,49 +33,42 @@ cd ../..
 
 
 
-# Creating MetalLB
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.3/config/manifests/metallb-native.yaml
-kubectl apply -f ipaddresspool.yaml
-kubectl apply -f l2advertisement.yaml 
-
-
-
-# Creating external ingress
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
-kubectl apply -f ingress-class.yaml
-kubectl apply -f ingress.yaml
-
-
-
 # Running deployment and service .yaml files
 cd dashboard
-sh _run_dashboard.sh
+kubectl apply -f deployment_service.yaml
 cd ..
 
 cd database
-sh _run_service.sh
+kubectl apply -f deployment_service.yaml
 cd ..
 
 cd db_connection
-sh _run_service.sh
+kubectl apply -f deployment_service.yaml
 cd ..
 
 cd epilepsy_prediction
-sh _run_service.sh
+kubectl apply -f deployment_service.yaml
 cd ..
 
 cd healt_center_web_page
-sh _run_service.sh
+kubectl apply -f deployment_service.yaml
 cd ..
 
 cd new_eeg_data_endpoint
-sh _run_service.sh
+kubectl apply -f deployment_service.yaml
 cd ..
 
 cd new_heartbeat_data_endpoint
-sh _run_service.sh
+kubectl apply -f deployment_service.yaml
 cd ..
 
 cd db_connection
-sh _run_service.sh
+kubectl apply -f deployment_service.yaml
 cd ..
+
+
+
+# Creating Ingress
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
+kubectl apply -f ingress-class.yaml
+kubectl apply -f ingress.yaml
