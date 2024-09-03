1819a3dd66512fc2d70dc3acf54850267e38b054
bertonfederico <federicoberton.fb@gmail.com>
2024-09-03 02:11:53 +0200

    Fixed naming error

--- _2_services/new_heartbeat_data_endpoint/docker/docker_builder.sh
+++ _2_services/new_heartbeat_data_endpoint/docker/docker_builder.sh
@@ -1,3 +1,3 @@
-docker build -t fberton98/new_heartbeata_endpoint:latest .
-docker push fberton98/new_heartbeata_endpoint:latest
-#docker run -p 5000:5000 fberton98/new_heartbeata_endpoint:latest
+docker build -t fberton98/new_heartbeat_data_endpoint:latest .
+docker push fberton98/new_heartbeat_data_endpoint:latest
+#docker run -p 5000:5000 fberton98/new_heartbeat_data_endpoint:latest
